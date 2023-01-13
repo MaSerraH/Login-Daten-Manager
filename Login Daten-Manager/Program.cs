983b5568e8936e199242a5c5a9c0819b35e1a717
Mauricio Serrano <maserranoh@gmx.at>
2023-01-13 15:40:29 +0100

    Projektdateien hinzufügen.

---
+++ Login Daten-Manager/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Daten_Manager
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            while (true)
+            {
+                Application.Run(new Form1());
+                break;
+            }
+            
+        }
+    }
+}
